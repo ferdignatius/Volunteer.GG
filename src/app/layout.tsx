251dd1f09639e8c015fbba8897019880c49cbbe9
AntiVeggie <Antiveg@users.noreply.github.com>
2025-05-25 14:55:41 +0700

    testing deployment build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,11 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar>
-          
-        </Navbar>
         {children}
       </body>
     </html>
-  );
+  )
 }
